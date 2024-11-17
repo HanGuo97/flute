b7c4c203aa81e8fb00556bd00c3269b3ccb60901
Han Guo <alex.guohan1106@gmail.com>
2024-11-17 14:04:02 -0500

    flute/ops: consistent naming

--- flute/ops.py
+++ flute/ops.py
@@ -5,12 +5,46 @@ def _qgemm_simple_abstract(
     input: torch.Tensor,
     weight: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
 ) -> torch.Tensor:
+    if not all([
+        input.ndim >= 2,
+        weight.ndim == 2,
+        scales.ndim == 2,
+        tables.ndim == 1,
+        tables2.ndim == 3,
+        workspace.ndim == 1,
+    ]):
+        raise ValueError
+
+    dtype = input.dtype
+    if dtype not in [torch.float16, torch.bfloat16]:
+        raise TypeError
+
+    if not all([
+        weight.dtype == torch.int16,
+        scales.dtype == dtype,
+        tables.dtype == dtype,
+        tables2.dtype == torch.float32,
+        workspace.dtype == torch.uint8,
+    ]):
+        raise TypeError
+
+    if not all([
+        weight.shape[1] == input.shape[-1],  # K
+        weight.shape[1] == scales.shape[1] * group_size,  # K
+        weight.shape[0] == int(num_bits * (scales.shape[0] / 16)),  # P
+        tables.shape[0] == 2 ** num_bits,
+        tables2.shape[0] == 2 ** num_bits,
+        tables2.shape[1] == 2 ** num_bits,
+        tables2.shape[2] == 1,
+    ]):
+        raise ValueError
+
     N = scales.shape[0]
     return torch.empty(
         input.shape[:-1] + (N,),
@@ -23,8 +57,8 @@ def _qgemm_simple_80_abstract(
     input: torch.Tensor,
     weight: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
@@ -33,8 +67,8 @@ def _qgemm_simple_80_abstract(
         input=input,
         weight=weight,
         scales=scales,
-        table=table,
-        table2=table2,
+        tables=tables,
+        tables2=tables2,
         workspace=workspace,
         num_bits=num_bits,
         group_size=group_size,
@@ -46,8 +80,8 @@ def _qgemm_simple_86_abstract(
     input: torch.Tensor,
     weight: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
@@ -56,8 +90,8 @@ def _qgemm_simple_86_abstract(
         input=input,
         weight=weight,
         scales=scales,
-        table=table,
-        table2=table2,
+        tables=tables,
+        tables2=tables2,
         workspace=workspace,
         num_bits=num_bits,
         group_size=group_size,
@@ -69,8 +103,8 @@ def _qgemm_simple_89_abstract(
     input: torch.Tensor,
     weight: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
@@ -79,8 +113,8 @@ def _qgemm_simple_89_abstract(
         input=input,
         weight=weight,
         scales=scales,
-        table=table,
-        table2=table2,
+        tables=tables,
+        tables2=tables2,
         workspace=workspace,
         num_bits=num_bits,
         group_size=group_size,
@@ -93,8 +127,8 @@ def _qgemm_raw_simple_80_abstract(
     weight: torch.Tensor,
     output: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
@@ -109,8 +143,8 @@ def _qgemm_raw_simple_86_abstract(
     weight: torch.Tensor,
     output: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
@@ -125,8 +159,8 @@ def _qgemm_raw_simple_89_abstract(
     weight: torch.Tensor,
     output: torch.Tensor,
     scales: torch.Tensor,
-    table: torch.Tensor,
-    table2: torch.Tensor,
+    tables: torch.Tensor,
+    tables2: torch.Tensor,
     workspace: torch.Tensor,
     num_bits: int,
     group_size: int,
